9b59e4c96cea434a03080755e4682359c58a1607
Jake Robbins <jrobbinsd@gmail.com>
2017-12-19 14:49:00 -0700

    refactored login code

--- db/api.js
+++ db/api.js
@@ -1,8 +1,20 @@
 var knex = require('./knex')
 
 module.exports = {
-  signIn: function() {
+  signIn: function(user) {
+		return knex('my_user').where('agentName', user.agentId)
   },
+
+	authLogin(userInput, agentInfo){
+		bcrypt.compare(userInput.password, agentInfo[0].password, function(err, response){ if (response === true){
+			res.render('assignment')
+		} else {
+			res.render('index', { title: 'gClassified', message: 'Incorrect login. Contents will self destruct' });
+		}
+		})
+	},
+
   signUp: function() {
+		return knex(my_user)
   }
 }
