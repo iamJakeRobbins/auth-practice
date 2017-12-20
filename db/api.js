65a0e3d785358117efa75557002fd457e124abf7
Jake Robbins <jrobbinsd@gmail.com>
2017-12-20 09:24:28 -0700

    refactored and added cookies

--- db/api.js
+++ db/api.js
@@ -1,28 +1,45 @@
 var knex = require('./knex')
+var bcrypt = require('bcrypt')
 
-module.exports = {
-  signIn: function(user) {
+
+  function signIn(user){
 		return knex('my_user').where('agentName', user.agentId)
-  },
+  }
 
-	authLogin(userInput, agentInfo){
+	function assign_cookie(agentInfo, req){
+		req.session.user = {
+			id: agentInfo[0].id
+		}
+	}
+
+	function authLogin(userInput, agentInfo,req,res){
 		bcrypt.compare(userInput.password, agentInfo[0].password, function(err, response){ if (response === true){
+			assign_cookie(agentInfo,req)
 			res.render('assignment')
 		} else {
 			res.render('index', { title: 'gClassified', message: 'Incorrect login. Contents will self destruct' });
 		}
 		})
-	},
+	}
 
-  signUp: function(newUser) {
+
+
+  function signUp(newUser) {
 		return knex('my_user').insert(newUser, 'id')
-  },
+  }
 
-	userCreation(agent){
+	function userCreation(agent){
 		if (agent[0].password === req.body.password) {
 			res.render('index', { title: 'gClassified', message: 'Password Must Be Hashed. Government Secrets are at Stake!' })
 		} else {
 			res.render('index', { title: 'gClassified', message: 'Sign Up Successful' })
 		}
 	}
-}
+
+	module.exports = {
+		authLogin,
+		assign_cookie,
+		signIn,
+		signUp,
+		userCreation
+	}
